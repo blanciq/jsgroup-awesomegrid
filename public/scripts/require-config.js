d8db803161d967b32f2dc6932a32c19400426bf2
wciula <wojciech.ciula@goyello.com>
2013-09-19 14:08:10 +0200

    Update require-config.js

--- public/scripts/require-config.js
+++ public/scripts/require-config.js
@@ -5,7 +5,8 @@
         baseUrl: "public/scripts",
         paths: {
             "jquery": "../lib/jQuery/jquery-1.9.1.min",
-            "awesomeGrid": "../lib/AwesomeGrid/scripts/AwesomeGrid.plugin"
+            "awesomeGrid": "../lib/AwesomeGrid/scripts/AwesomeGrid.plugin",
+            "ajaxCalls": "../Scripts/ajaxCalls"
         },
         shim: {
             "awesomeGrid": {
